caa29d746430ff36ffcffe4578d27f2a3344fd0a
kirumbastacy25l <minniebooh7@gmail.com>
2025-02-04 14:06:52 +0300

    fourth commit

---
+++ Variable.py
@@ -0,0 +1,19 @@
+# Variable is a container used to store a value
+
+location = "westlands"
+firstname = "Peter"
+lastname = "kiarie"
+school = "eMobilis"
+x = 39
+y = 2
+z = 78
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname, lastname)
+print("My fullname is", firstname,lastname)
+print(school,"is a boarding school")
+print(x + y)
+print("The product of",x,"and",y,"is",z)
---
+++ introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to display an output
+
+print("Peter")
+print("eMobilis")
+print(100)
+print(10+20)
