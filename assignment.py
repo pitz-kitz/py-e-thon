0bc08af07d1fef8680a5d3f98c17b462049907de
pitz-kitz <kiariepeter690@gmail.com>
2025-02-05 16:47:57 +0300

    sixth commit

--- assignment.py
+++ assignment.py
@@ -3,3 +3,21 @@
 
 # 2. A python program that checks whether
 # letter is a vowel or a consonant
+
+year = 2024
+
+if (year % 4 == 0 and year % 100 != 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
+
+
+    #2
+
+letter = "p"
+if letter in ("a","e","i","o","u"):
+    print("vowel")
+elif letter in ("A","E","I","O","U"):
+    print("vowel")
+else:
+    print("consonant")
